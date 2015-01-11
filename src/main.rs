a471e55687f2112cd749a268dedd2357d0a12c73
Dan Stowell <stowell@fastmail.fm>
2015-01-10 21:34:27 -0500

    let us sort random u64s
    Change-Id: I1fc87255645302209901553c25d18f0236d7f952

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,23 @@
 use std::cmp;
-use std::io;
-use std::io::fs::PathExtensions;
 use std::iter;
-use std::mem;
-use std::num;
-use std::os;
+use std::rand;
 
 fn main() {
-    for arg in os::args()[1..].iter() {
-        let path = Path::new(arg);
-        let mut file = io::File::open(&path).unwrap();
-        let nbytes: usize = num::from_u64(path.stat().unwrap().size).unwrap();
-        let nitems = nbytes / mem::size_of::<u64>();
+    let nitems = 1024;
+    let mut u64s = Vec::with_capacity(nitems);
+    for _ in range(0, nitems) {
+        let u64: u64 = rand::random();
+        u64s.push(u64);
+    }
 
-        let mut u64s = Vec::with_capacity(nitems);
-        loop {
-            let u64 = match file.read_le_u64() {
-                Ok(h) => h,
-                Err(_) => break,
-            };
-            u64s.push(u64);
-        }
+    const STEP: usize = 4;
+    for start in iter::range_step(0, nitems, STEP) {
+        let end = cmp::min(start + STEP, nitems);
+        let view = u64s.slice_mut(start, end);
+        view.sort();
+    }
 
-        const STEP: usize = 4;
-        for start in iter::range_step(0, nitems, STEP) {
-            let end = cmp::min(start + STEP, nitems);
-            let view = u64s.slice_mut(start, end);
-            view.sort();
-        }
-
-        for &u64 in u64s.iter() {
-            println!("{}", u64);
-        }
+    for &u64 in u64s.iter() {
+        println!("{}", u64);
     }
 }
